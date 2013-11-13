b73a61d4ab5ec1855f7cc9b8508fca40d4208825
augustinIAGL2013 <augustinp2002@gmail.com>
2013-11-13 09:42:29 +0100

    rajout de regles sonar

--- SonarRulesTest/src/main/java/testSonar/SonarRules.java
+++ SonarRulesTest/src/main/java/testSonar/SonarRules.java
@@ -2,28 +2,29 @@ package testSonar;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Vector;
-
 /*
  * Unused Imports
  */
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 
 class Door{
 	void open(){
-		
+
 	}
 	void close(){
-		
+
 	}
 }
 
+@SuppressWarnings("serial")
 public class SonarRules extends ConcurrentHashMap<String, String>{
 
 	private int myProperty;
+	public int[] array;
 
 	/* Naming - Suspicious constant field name */
 	double PI = 3.16;
@@ -156,7 +157,7 @@ public class SonarRules extends ConcurrentHashMap<String, String>{
 		test = "test2";
 		return test.toString();
 	}
-	
+
 	/*
 	 * Avoid Catching Throwable+Unconditional If Statement
 	 */
@@ -169,44 +170,45 @@ public class SonarRules extends ConcurrentHashMap<String, String>{
 				e.printStackTrace();
 			} 
 		}
-		
+
 	}
-	
+
 	/*
 	 * Equals Hash Code
+	 * Naming - Suspicious equals method name
 	 */
 
 	public boolean equals(Object o) {
 		return true;
 	}
-	
+
 
 	/*
 	 * Using monitor style wait methods on util.concurrent abstraction
 	 */
-	
+
 	void methodCallWait() throws InterruptedException{
 		wait();
 	}
 	/*
 	 * Performance - Use the nextInt method of Random rather than nextDouble to generate a random integer
 	 */
-	
+
 	int rand(){
 		Random ran = new Random();
 		return ((int)(ran.nextDouble()*10));
 	}
-	
+
 	/*
 	 * Switch statement found where one case falls through to the next case
 	 */
 	void methodCallSwitch(){
 		switch (rand()){
-			case 42 :
-				System.out.println("42 c'est la classe !");
-			case 0:
-				System.out.println("c'est null");
-				break;
+		case 42 :
+			System.out.println("42 c'est la classe !");
+		case 0:
+			System.out.println("c'est null");
+			break;
 		}
 	}
 	/*
@@ -218,14 +220,14 @@ public class SonarRules extends ConcurrentHashMap<String, String>{
 	void methodWhoCallPreviousMethd(){
 		ArrayList list = (ArrayList<Integer>)methodeWhoReaturnObject();
 	}
-	
+
 	/*
 	 * Naming - Suspicious equals method name
 	 */
 	public  boolean equals(SonarRules o) {
 		return true;
 	}
-	
+
 	/*
 	 * Correctness - A known null value is checked to see if it is an instance of a type
 	 */
@@ -233,12 +235,122 @@ public class SonarRules extends ConcurrentHashMap<String, String>{
 		Object maVariableNull = null;
 		return maVariableNull instanceof Object;
 	}
-	
+
 	/*
 	 * Correctness - close() invoked on a value that is always null
 	 */
 	public void closeNull(){
 		Door maVariableNull = null;
 		maVariableNull.close();
+	}
+
+	/*
+	 * Performance - Private method is never called
+	 */
+	private void neverCalledMethod(){
+		//nevercalled method
+	}
+
+	/*
+	 * Security - Array is stored directly
+	 */
+	public void addArray(int[] list){
+		array=list;
+	}
+
+	/*
+	 * Security - Method returns internal array
+	 */
+	public int[] returnArray(){
+		return this.array;
+	}
+
+	/*
+	 * Missing Break In Switch
+	 */
+	public void swithcasemeth(){
+		switch (array[0]) {
+		case 1:
+			array[0]++;
+			//error 
+
+		case 2:
+			array[0]++;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+
+	/*
+	 * Unconditional If Statement
+	 */
+	public void iftest(){
+		if(true) ;
+	}
+
+
+
+
+	/*
+	 * Naming - Suspicious equals method name
+	 */
+	public boolean equals(String s) {
+		// oops, this probably was supposed to be equals(Object)
+		return true;
+	}
+
+	/*
+	 * 	Performance - Method concatenates strings using + in a loop
+	 */
+	public void perfplus(){
+		// This is bad
+		int[] field=new int[]{0,1,2,3,4,5,6};
+		String s = "";
+		for (int i = 0; i < field.length; ++i) {
+			s = s + field[i];
+		}
+	}
+
+	/*
+	Performance - Method invokes inefficient Number constructor; use static valueOf instead
+	 */
+	public int useNonOptimisedIntegerMethod(){
+		return new Integer(5); 
+	}
+
+	/*
+	 * Dodgy - Useless control flow
+	 */
+	public void ifStatementEmpty(int a){
+		if(a==0){
+			//do something
+		}
+	}
+
+	/*
+	 * Misplaced Null Check
+	 */
+	public void nullcheck(){
+		String object1="aa",object2="er";
+		if (object1!=null && object2.equals(object1)) { 
+			//do something
+		}      
+	}
+
+
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+		SonarRules ap=new SonarRules();
+		ap.addArray(new int[]{1,2});
+		ap.returnArray();
+		ap.swithcasemeth();
+		ap.iftest();
+		ap.perfplus();
+		ap.useNonOptimisedIntegerMethod();
+		ap.nullcheck();
 	}
 }
